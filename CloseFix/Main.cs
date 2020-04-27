15b9f33cd857045e8d89341b3aa442909e30fc78
404 <404alt@gmail.com>
2020-04-26 22:47:20 -0500

    Add project files.

---
+++ CloseFix/Main.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloseFix
+{
+    public static class BuildInfo
+    {
+        public const string Name = "CloseFix"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "404#0004"; // Author of the Mod.  (Set as null if none)
+        public const string Company = "I am not a company -Kappa-"; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = "https://github.com/l-404-l"; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class CloseFix404 : MelonMod
+    {
+        public override void OnApplicationQuit()
+        {
+            CloseGameAfterConfig().GetAwaiter().GetResult();
+        }
+
+        public async Task CloseGameAfterConfig()
+        {
+            MelonModLogger.Log("Closing your game.");
+            // Will add more for world glitching soon
+            await Task.Delay(1000);
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+
+}
---
+++ CloseFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using System;
+
+[assembly: AssemblyTitle(CloseFix.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(CloseFix.BuildInfo.Company)]
+[assembly: AssemblyProduct(CloseFix.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + CloseFix.BuildInfo.Author)]
+[assembly: AssemblyTrademark(CloseFix.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(CloseFix.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(CloseFix.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(CloseFix.CloseFix404), CloseFix.BuildInfo.Name, CloseFix.BuildInfo.Version, CloseFix.BuildInfo.Author, CloseFix.BuildInfo.DownloadLink)]
+[assembly: MelonModGame("VRChat", "VRChat")]
+//[assembly: MelonModGame("", "")]
